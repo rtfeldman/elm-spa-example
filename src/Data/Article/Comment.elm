46982a4b3faa31eff044c003aef8e77f2ade26c4
Richard Feldman <richard.t.feldman@gmail.com>
2018-03-13 09:56:54 -0400

    Use Posix over Date

--- src/Data/Article/Comment.elm
+++ src/Data/Article/Comment.elm
@@ -1,17 +1,17 @@
 module Data.Article.Comment exposing (Comment, CommentId, commentIdDecoder, decoder, idToString)
 
 import Data.Article.Author as Author exposing (Author)
-import Date exposing (Date)
 import Json.Decode as Decode exposing (Decoder)
-import Json.Decode.Extra
 import Json.Decode.Pipeline exposing (custom, decode, required)
+import Time exposing (Posix)
+import Util
 
 
 type alias Comment =
     { id : CommentId
     , body : String
-    , createdAt : Date
-    , updatedAt : Date
+    , createdAt : Posix
+    , updatedAt : Posix
     , author : Author
     }
 
@@ -25,8 +25,8 @@ decoder =
     decode Comment
         |> required "id" commentIdDecoder
         |> required "body" Decode.string
-        |> required "createdAt" Json.Decode.Extra.date
-        |> required "updatedAt" Json.Decode.Extra.date
+        |> required "createdAt" Util.dateStringDecoder
+        |> required "updatedAt" Util.dateStringDecoder
         |> required "author" Author.decoder
 
 

1c4b6a688b4f12c10046aaf55469199374655cf3
Richard Feldman <richard.t.feldman@gmail.com>
2018-07-21 15:08:41 -0400

    Reorganize some comments

--- src/Route.elm
+++ src/Route.elm
@@ -11,7 +11,7 @@ import Username exposing (Username)
 
 
 
--- ROUTING --
+-- ROUTING
 
 
 type Route
@@ -40,6 +40,29 @@ parser =
         , Parser.map NewArticle (s "editor")
         , Parser.map EditArticle (s "editor" </> Slug.urlParser)
         ]
+
+
+
+-- PUBLIC HELPERS
+
+
+href : Route -> Attribute msg
+href targetRoute =
+    Attr.href (routeToString targetRoute)
+
+
+replaceUrl : Nav.Key -> Route -> Cmd msg
+replaceUrl key route =
+    Nav.replaceUrl key (routeToString route)
+
+
+fromUrl : Url -> Maybe Route
+fromUrl url =
+    -- The RealWorld spec treats the fragment like a path.
+    -- This makes it *literally* the path, so we can proceed
+    -- with parsing as if it had been a normal path all along.
+    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+        |> Parser.parse parser
 
 
 
@@ -82,26 +105,3 @@ routeToString page =
                     [ "editor", Slug.toString slug ]
     in
     "#/" ++ String.join "/" pieces
-
-
-
--- PUBLIC HELPERS --
-
-
-href : Route -> Attribute msg
-href targetRoute =
-    Attr.href (routeToString targetRoute)
-
-
-replaceUrl : Nav.Key -> Route -> Cmd msg
-replaceUrl key route =
-    Nav.replaceUrl key (routeToString route)
-
-
-fromUrl : Url -> Maybe Route
-fromUrl url =
-    -- The RealWorld spec treats the fragment like a path.
-    -- This makes it *literally* the path, so we can proceed
-    -- with parsing as if it had been a normal path all along.
-    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
-        |> Parser.parse parser

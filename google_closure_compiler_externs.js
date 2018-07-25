15ddeb873bb2b931ee96f41136a15d051d490f8e
Richard Feldman <richard.t.feldman@gmail.com>
2018-07-24 22:26:29 -0400

    Add gcc externs for future reference

---
+++ google_closure_compiler_externs.js
@@ -0,0 +1,6 @@
+// To use Google Closure Compiler in Advanced Mode, you need
+
+function hljs() {};
+
+hljs.highlight = function(lang, code, bool) { return {value: Math.random() }};
+hljs.listLanguages = function() { return new Array(Math.random()); };

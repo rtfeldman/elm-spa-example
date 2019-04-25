76af9f391c240e5f10af4bc7609ef9c94532820e
Tadeusz Łazurski <tadeusz@lazurski.pl>
2019-04-25 19:00:28 +0200

    Make Login and Register pages trigger active link in the nav bar
    Previously they were tagged with Page.Other, but the Page type has tags for
    Login and Register, so why not use them. The effect is that now navigating to
    these pages highlights the appropriate link in the nav bar.

--- src/Main.elm
+++ src/Main.elm
@@ -26,6 +26,7 @@ import Username exposing (Username)
 import Viewer exposing (Viewer)
 
 
+
 -- NOTE: Based on discussions around how asset management features
 -- like code splitting and lazy loading have been shaping up, it's possible
 -- that most of this file may become unnecessary in a future release of Elm.
@@ -85,10 +86,10 @@ view model =
             viewPage Page.Home GotHomeMsg (Home.view home)
 
         Login login ->
-            viewPage Page.Other GotLoginMsg (Login.view login)
+            viewPage Page.Login GotLoginMsg (Login.view login)
 
         Register register ->
-            viewPage Page.Other GotRegisterMsg (Register.view register)
+            viewPage Page.Register GotRegisterMsg (Register.view register)
 
         Profile username profile ->
             viewPage (Page.Profile username) GotProfileMsg (Profile.view profile)
